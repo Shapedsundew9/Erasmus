7d10e3120af3f4ac544109d45f1acb0356650748
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2020-06-01 18:59:09 -0400

    Lots of additions around gene_pool & worker - again

--- microbiome/genetics/gene_pool.py
+++ microbiome/genetics/gene_pool.py
@@ -11,6 +11,8 @@ from logging import getLogger
 from .genomic_library import genomic_library
 from .genomic_library_entry_validator import NULL_GC
 from tempfile import NamedTemporaryFile
+from graph_tool import Graph
+from numpy import int64
 
 
 # The gene_pool organises genetic codes for a worker. Each worker has its own gene pool.
@@ -29,7 +31,6 @@ class gene_pool():
 
 
     __gl = None
-    __UNIQUE_CHARS = 16
     __CALLABLE_FILE_HEADER = "# Erasmus GP Gene Pool\n\n\n"
     __logger = getLogger(__name__)
 
@@ -40,13 +41,13 @@ class gene_pool():
         self.callables_prefix = callables_prefix
         self.__callable_file_ptr = None
         gene_pool.__logger.info("Initial gene pool query: %s", str(query))
-        self.__active_gcs = gene_pool.__gl.load(query)
-        self.__gene_pool = set()
-        self.__create_callables()
+        self.__gene_pool = {gc['signature']: gc for gc in gene_pool.__gl.load(query)}
+        self.graph = None
+        self.__create_callables(self.__gene_pool.keys())
 
 
     def __function_name(self, signature):
-        return "gc_" + signature[:gene_pool.__UNIQUE_CHARS]
+        return "gc_" + signature
 
 
     def __write_arg(self, iab, c):
@@ -69,23 +70,57 @@ class gene_pool():
             self.__callable_file_ptr.write(gc['meta_data']['function']['python3']['0']['callable'] + "\n\n")
 
 
-    def __unique_key(self, signature):
-        return signature[:gene_pool.__UNIQUE_CHARS]
+    def __add_to_graph(self, gc, parent=None):
+        if 'vertex' not in gc:
+            gc['vertex'] = self.graph.add_vertex()
+            self.graph.vertex_properties.signature[gc['vertex']] = gc['signature']
+        if not parent is None:
+            if 'vertex' not in parent:
+                parent['vertex'] = self.graph.add_vertex()
+                self.graph.vertex_properties.signature[parent['vertex']] = parent['signature']
+            if not parent['vertex'] in gc['vertex'].get_in_neighbors(): self.graph.add_edge(parent['vertex'], gc['vertex'])
 
 
-    def __recurse_gcs(self, gc):
-        if not self.__unique_key(gc['signature']) in self.__gene_pool:
-            self.__gene_pool.add(self.__unique_key(gc['signature']))
+    def __recurse_gcs(self, gc, parent=None):
+        self.__add_to_graph(gc, parent)
+        if not gc['signature'] in self.__gene_pool:
+            self.__gene_pool[gc['signature']] = gc
             self.__write_gc_function(gc)
-            if not 'function' in gc['meta_data']:  
-                if not self.__unique_key(gc['gca']) in self.__gene_pool and gc['gca'] != NULL_GC: self.__recurse_gcs(gene_pool.__gl[gc['gca']][0])
-                if not self.__unique_key(gc['gcb']) in self.__gene_pool and gc['gcb'] != NULL_GC: self.__recurse_gcs(gene_pool.__gl[gc['gcb']][0])
+            if not gc['gca'] in self.__gene_pool and gc['gca'] != NULL_GC: self.__recurse_gcs(gene_pool.__gl[gc['gca']][0], gc)
+            if not gc['gcb'] in self.__gene_pool and gc['gcb'] != NULL_GC: self.__recurse_gcs(gene_pool.__gl[gc['gcb']][0], gc)
 
 
-    def __create_callables(self):
+    def __create_callables(self, active_gcs):
+        self.graph = Graph()
+        self.graph.vertex_properties['signature'] = self.graph.new_vp('string')
         self.__callable_file_ptr = NamedTemporaryFile(mode='w', suffix='.py', prefix=self.callables_prefix, delete=self.delete_callables_file)
         gene_pool.__logger.debug("Gene pool file created: %s", self.__callable_file_ptr.name)
         self.__callable_file_ptr.write(gene_pool.__CALLABLE_FILE_HEADER)    
-        for gc in self.__active_gcs:
+        for gc in active_gcs:
             if gc['signature'] != NULL_GC: self.__recurse_gcs(gc)
         self.__callable_file_ptr.close()
+
+
+    def __edge_list(self, signature, incoming_edge=None):
+        edge_list = [] if incoming_edge is None else [(self.__gene_pool[signature], incoming_edge)]
+        for edge in self.__gene_pool[signature]['vertex'].get_out_edges():
+            edge_list.extend(self.__edge_list(self.graph.vertex_properties.signature[edge.target()], edge))
+        return edge_list
+
+
+    def edge_list(self, signature):
+        if 'edge_list' not in self.__gene_pool['signature']: self.__gene_pool['signature']['edge_list'] = self.__edge_list(signature)
+        return self.__gene_pool['signature']['edge_list']
+
+
+    # Creation of a new GC within an existing GC (signature) requires
+    # modification of all of the GC's in between 
+    def create(self, new, signature, parent_a, parent_b=None):
+        if parent_b is None:
+            new['generation'] = parent_a['generation'] + 1
+            new['properties'] = parent_a['properties']
+            new['meta_data'] = {'parents': [parent_a['signature']]}
+        else:
+            new['generation'] = max((parent_a['generation'], parent_b['generation'])) + 1
+            new['properties'] = {k: parent_a[k] or parent_b[k] for k in parent_a['properties'}
+            new['meta_data'] = {'parents': [parent_a['signature']]}
--- microbiome/worker.py
+++ microbiome/worker.py
@@ -15,6 +15,8 @@ from .worker_registry_validator import worker_registry_validator
 from copy import deepcopy
 from logging import getLogger
 from math import isclose
+from random import choices, setstate
+from numpy import float32, array, amax, amin
 
 
 class worker():
@@ -41,6 +43,10 @@ class worker():
         self.gene_pool = None
 
 
+    def __function_not_found(self, name):
+        worker.__logger.error("The dynamically determined function %s cannot be found.", name)
+
+
     def __valid_work(self):
         count = 0
         for signature, fitness in self.__calculate_fitness().items():
@@ -61,15 +67,100 @@ class worker():
         return {}
 
 
-
     def __stop_criteria_met(self):
         # TODO: Implement
         return True
 
 
-    def __mutate(self):
+    # TODO: Need a lot of diagnostics around this to see how it behaves
+    # Need to identify the instance of the a sub-GC to modifiy which is defined by the incoming
+    # edge in the top level GC graph
+    def __select_edge(self, signature, dpm, gpm):
+        generations = []
+        depths = []
+        for gc, edge in self.gene_pool.edge_list(signature):
+            generations.append(gc['generation'])
+            depths.append(gc['code_depth'])
+        generations = array(generations, dtype=float32)
+        depths = array(depths, dtype=float32)
+        generations = float32(1.0) + (float32(1.0) - generations / amax(generations)) * gpm
+        depths = float32(1.0) + (float32(1.0) - depths / amax(depths)) * dpm
+        return choices(self.gene_pool.edge_list(signature), generations + depths)
+
+
+    # To duplicate a GC a new GC is added that takes the same inputs as the
+    # original GC and connects them to two copies in GCA & GCB
+    def __gc_duplication(self, signature):
+        cf = self.work['evolution_parameters']['asexual']['config']['duplication']
+        gc, edge = self.__select_edge(gc_list, cf['dpm'], cf['gpm'])
+
+        # Define the new GC
+        new_inputs = [['I', i] for i in range(gc['num_inputs'])]
+        new_gc['graph'] = {
+            'A': new_inputs,
+            'B': new_inputs,
+            'O': [[gcab, i] for gcab in ['A', 'B'] for i in range(gc['num_outputs'])]
+        }
+        new_gc['gca'] = gc['signature']
+        new_gc['gcb'] = gc['signature']
+        new_gc['code_depth'] = gc['code_depth'] + 1
+        new_gc['codon_depth'] = gc['codon_depth']
+        new_gc['num_codes'] = gc['num_codes'] * 2
+        new_gc['num_unique_codes'] = gc['num_unique_codes']
+        new_gc['raw_num_codons'] = gc['raw_num_codons'] * 2
+        new_gc['creator'] = self.config['creator']
+
+        # Adding to the gene pool automatically creates the new top level GC & all those in between
+        return self.gene_pool.create(new_gc, signature, gc)
+
+
+    def __gc_addition(self, signature):
+        cf = self.work['evolution_parameters']['asexual']['config']['addition']
+        return {}
+
+
+    def __gc_subtraction(self, signature):
+        cf = self.work['evolution_parameters']['asexual']['config']['subtraction']
+        return {}
+
+
+    def __gc_exchange(self, signature):
+        cf = self.work['evolution_parameters']['asexual']['config']['exchange']
+        return {}
+
+
+    def __gc_rewire(self, signature):
+        cf = self.work['evolution_parameters']['asexual']['config']['rewire']
+        return {}
+
+
+    def __gc_adjust_constant(self, signature):
+        cf = self.work['evolution_parameters']['asexual']['config']['adjust_constant']
+        return {}
+
+
+    def __asexual_reproduction(self, signature):
+        worker.__logger.debug("Asexual reproduction for individual %s", signature)
+        cf = self.work['evolution_parameters']['asexual']['config']
+        method = '__gc_' + choices(list(cf.keys()), [v['weight'] for v in cf.values()]):
+        return getattr(self, method, lambda: self.__function_not_found(method))(signature)
+
+
+    def __sexual_reproduction(self, signature):
+        worker.__logger.debug("Sexual reproduction for individual %s", signature)
         # TODO: Implement
         return {}
+
+
+    def __evolve(self):
+        cf = self.work['evolution_parameters']
+        new_population = {}
+        for signature in self.work['population_dict'].keys():
+            if choices([True, False], [cf['asexual']['weight'], cf['sexual']['weight']])
+                new_population[self.__asexual_reproduction(signature)] = 0.0
+             else
+                new_population[self.__sexual_reproduction(signature)] = 0.0
+        return new_population
 
 
     def __cull(self):
@@ -90,7 +181,7 @@ class worker():
     #
     # Loop
     # ----
-    # 3. Mutate and assess (this updates the gene pool)
+    # 3. Evolve state and assess (this updates the gene pool)
     # 4. Lock the work definition & read it (may have been updated by another worker)
     # 5. Merge generations
     # 6. Cull
@@ -103,6 +194,7 @@ class worker():
 
         # Initialise
         self.work = worker.__work_registry.load([{'signature': self.registration_document['work']}])[0]
+        setstate(self.work['evolution_parameters'])
         existing_population = len(self.work['population_dict'])
         if existing_population: self.work['initial_query'] = [{'signature': list(self.work['population_list'].keys())}]
         self.gene_pool = gene_pool(self.work['initial_query'], self.config['delete_gene_pool'], self.config['gene_pool_prefix'])
@@ -112,7 +204,7 @@ class worker():
         
         # Loop
         while not self.__stop_criteria_met():
-            self.work['population_dict'] = self.__mutate()
+            self.work['population_dict'] = self.__evolve()
             self.work['population_dict'] = self.__calculate_fitness()
             self.work['population_dict'].update(worker.__work_registry.lock_and_load([{'signature': self.registration_document['work']}], ['population_dict'])[0])
             self.work['population_dict'] = self.__cull()
